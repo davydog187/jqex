f471d8a7627ccbfc1763eeb577a2e29d4069bc79
Dave Lucia <davelucianyc@gmail.com>
2020-07-29 08:20:19 -0400

    read all input

--- lib/jqex.ex
+++ lib/jqex.ex
@@ -4,10 +4,15 @@ defmodule Jqex do
   """
 
   def main(_args) do
-    IO.stream(:stdio, :line)
-    |> Enum.each(fn line ->
-      line |> Jason.decode!() |> inspect() |> IO.write()
-      IO.write("\n")
-    end)
+    input = IO.read(:stdio, :all)
+
+    case Jason.decode(input) do
+      {:ok, json} ->
+        json |> inspect(pretty: true) |> IO.write()
+        IO.write("\n")
+
+      {:error, error} ->
+        IO.inspect(error)
+    end
   end
 end
